54241f36630c9c0715d764ee3f5b823cfdee16ad
roomstyle28-del <roomstyle28@gmail.com>
2026-05-02 17:49:39 +0700

    Update and rename pwabuilder-sw.js to sw.js

---
+++ sw.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = 'dompetku-v1';
+const ASSETS_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png'
+];
+
+// Proses Install (Menyimpan file ke memori HP)
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+// Proses Fetch (Mengambil data dari memori jika offline)
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+// Membersihkan cache lama jika ada update
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
